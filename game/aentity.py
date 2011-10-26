a10caf377b50535e6e5baff8b4d3130a374e57e3
theGraffo <theGraffo@1f0f5e7e-055b-f282-4f42-4ee5b0881360>
2011-10-26 06:11:12 +0000

    Minor changes.

--- game/aentity.py
+++ game/aentity.py
@@ -1,6 +1,11 @@
 from engine.entity import Entity
 from engine.graphics import Stamp
+import random
 
 class Dude(Entity):
     def init(self):
         self.graphic = Stamp(self.game.gfxEngine, "dude.png")
+
+    def onStep(self):
+	self.x += random.randint(-10, 10)
+
